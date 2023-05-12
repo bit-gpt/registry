bd650b41d3abf5990d28e5cefad6dc5153c0e439
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-12 14:09:55 +0200

    serving logos too

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from markdown import markdown
 from pydantic import BaseModel
 
@@ -32,6 +33,8 @@ app.add_middleware(
 
 manifests = []
 
+app.mount("/logos", StaticFiles(directory="."), name="logos")
+
 
 @app.on_event("startup")
 async def load_manifests():
@@ -42,6 +45,7 @@ async def load_manifests():
         if os.path.isdir(folder_path) and "prem" in folder_path:
             manifest_path = os.path.join(folder_path, "manifest.json")
             readme_path = os.path.join(folder_path, "README.md")
+            logo_path = os.path.join(folder_path, "logo.svg")
 
             with open(manifest_path) as f:
                 manifest = json.load(f)
@@ -49,6 +53,11 @@ async def load_manifests():
             with codecs.open(readme_path, "r", encoding="utf-8") as f:
                 readme_content = f.read()
                 manifest["documentation"] = markdown(readme_content)
+
+            if os.path.exists(logo_path):
+                manifest["icon"] = f"/logos/{folder}/logo.svg"
+            else:
+                manifest["icon"] = None
 
             manifests.append(manifest)
 
