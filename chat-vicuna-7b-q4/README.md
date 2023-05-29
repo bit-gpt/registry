a8fc72a6f175d27555af4935e9413e5c4c0740ed
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-29 09:39:17 +0200

    Initial commit

---
+++ chat-vicuna-7b-q4/README.md
@@ -0,0 +1,13 @@
+# Documentation
+
+## Description
+
+Vicuna is an open-source chatbot, fine-tuned on LLaMA using conversations from ShareGPT. Developed collaboratively by a team from UC Berkeley, CMU, Stanford, and UC San Diego, this auto-regressive language model aims to advance research in large language models and chatbots. Primarily designed for researchers and hobbyists in AI and NLP, the model undergoes preliminary evaluations using diverse questions and GPT-4 for output judgment. The current model has been 4-bit quantized using ggml framework. [Learn More](https://vicuna.lmsys.org/)
+
+## Hardware Requirements
+
+## Example Usage
+
+## Fine Tuning Instructions & Cost
+
+## Inference Benchmarks
---
+++ generate.py
@@ -0,0 +1,34 @@
+import codecs
+import json
+import os
+
+from markdown import markdown
+
+APPS = ["chat", "store", "copilot", "embeddings"]
+
+manifests = []
+for folder in os.listdir("."):
+    folder_path = os.path.join(".", folder)
+    if os.path.isdir(folder_path) and any(s in folder_path for s in APPS):
+        manifest_path = os.path.join(folder_path, "manifest.json")
+        readme_path = os.path.join(folder_path, "README.md")
+        logo_path = os.path.join(folder_path, "logo.svg")
+
+        with open(manifest_path) as f:
+            manifest = json.load(f)
+
+        with codecs.open(readme_path, "r", encoding="utf-8") as f:
+            readme_content = f.read()
+            manifest["documentation"] = markdown(readme_content)
+
+        if os.path.exists(logo_path):
+            manifest[
+                "icon"
+            ] = f"https://raw.githubusercontent.com/premAI-io/prem-registry/main/{folder}/logo.svg"
+        else:
+            manifest["icon"] = None
+
+        manifests.append(manifest)
+
+with open("manifests.json", "w") as f:
+    json.dump(manifests, f)
